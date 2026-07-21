6ffa91aa598f4f71e0ef7d0f0ca24013ced2175d
ferafra595 <ferafra595@gmail.com>
2026-07-21 18:33:30 +0200

    Update [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -25,7 +25,7 @@ async function setExtendedRole() {
 
 async function hashPassword(password, salt = crypto.randomUUID()) {
   const key = await crypto.subtle.importKey('raw', new TextEncoder().encode(password), 'PBKDF2', false, ['deriveBits']);
-  const bits = await crypto.subtle.deriveBits({ name: 'PBKDF2', salt: new TextEncoder().encode(salt), iterations: 120000, hash: 'SHA-256' }, key, 256);
+  const bits = await crypto.subtle.deriveBits({ name: 'PBKDF2', salt: new TextEncoder().encode(salt), iterations: 100000, hash: 'SHA-256' }, key, 256);
   const hash = [...new Uint8Array(bits)].map(b => b.toString(16).padStart(2, '0')).join('');
   return `${salt}:${hash}`;
 }
